5f39d779896041466eb7ec70a41e02d1dd193c6b
Akvir1stone <minnikaiev@mail.ru>
2023-11-26 16:52:47 +0300

    1

--- news_portal_f/news/models.py
+++ news_portal_f/news/models.py
@@ -4,31 +4,51 @@ from django.db import models
 
 
 class Authors(models.Model):
-    # one to one User
-    # rating
-    pass
+    # TODO one to one User
+    rating = models.IntegerField()
+
+    def update_rating(self):
+        pass
 
 
 class Category(models.Model):
     CatName = models.CharField(max_length=50, unique=True)
-    pass
 
 
 class Post(models.Model):
-    # one to many Author
-    articleORnews = models.BooleanField()  # article true / news false
+    # TODO one to many Author
+    articleORnews = models.BooleanField()  # article true / news false # TODO try do without bool, but instead with char field
     date_time = models.DateTimeField(auto_now_add=True)
     m_to_m_cat = models.ManyToManyField(Category, through="PostCategory")
     name = models.CharField()
     text = models.TextField()
     rating = models.IntegerField()
-    pass
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
+
+    def preview(self):
+        pass
 
 
 class PostCategory(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    pass
+    at_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # TODO one to many user
+    c_text = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
 
