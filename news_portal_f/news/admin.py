f4460d7ea89dfb5a8f4198dba810fb50537f6d62
Akvir1stone <minnikaiev@mail.ru>
2024-01-06 12:03:10 +0300

    1

--- news_portal_f/news/admin.py
+++ news_portal_f/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .news.models import Authors, Category, Post, PostCategory, Comment
+
+
+admin.site.register(Authors)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
 
 # Register your models here.
--- news_portal_f/news/views.py
+++ news_portal_f/news/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .news.models import Authors, Category, Post, PostCategory, Comment
 
-# Create your views here.
+
+class NewsList(Post):
+    model = Post
+    ordering = 'date_time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
