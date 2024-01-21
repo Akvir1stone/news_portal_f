4fe8aa2148c951d8202d534b3b5dd941ce650f49
Akvir1stone <minnikaiev@mail.ru>
2024-01-21 18:27:14 +0300

    1

--- news_portal_f/news_portal_f/urls.py
+++ news_portal_f/news_portal_f/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from news import models
+# from news import models
 
 urlpatterns = [
     path('admin/', admin.site.urls),
