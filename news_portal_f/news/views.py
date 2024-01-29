522f819a89f8b5732fdc9efdc617b2a3eea1fa5b
Akvir1stone <minnikaiev@mail.ru>
2024-01-29 19:05:24 +0300

    1

--- news_portal_f/news/views.py
+++ news_portal_f/news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Authors, Category, Post, PostCategory, Comment
+from django.core.paginator import Paginator
 
 
 class NewsList(ListView):
@@ -8,6 +10,7 @@ class NewsList(ListView):
     ordering = '-date_time'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 2
 
 
 class PostView(DetailView):
